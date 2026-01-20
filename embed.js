62dde69c7c840536ae0d1fd0bd38d63fc37464b5
ta26065 <atkins.tom@outlook.com>
2026-01-20 17:04:02 +0000

    Update embed.js

--- embed.js
+++ embed.js
@@ -3,15 +3,13 @@
     var iframes = document.querySelectorAll('iframe[data-senedd-widget="true"]');
     if (!iframes.length) return;
 
-    function onMessage(e) {
+    window.addEventListener("message", function (e) {
       if (!e || !e.data || e.data.type !== "SENEDD_WIDGET_HEIGHT") return;
 
       for (var i = 0; i < iframes.length; i++) {
         iframes[i].style.height = (e.data.height + 2) + "px";
       }
-    }
-
-    window.addEventListener("message", onMessage);
+    });
   }
 
   if (document.readyState === "loading") {
